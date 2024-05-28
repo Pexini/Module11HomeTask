9b98e8c9d8de1e0eba39aebdb9451dc6e638bdaa
Pavel Davydov <Davydov.works@gmail.com>
2024-05-28 04:16:08 -0400

    First tesk Meddle

--- src/main/java/Middle/FirstVolumeWorlds/Main.java
+++ src/main/java/Middle/FirstVolumeWorlds/Main.java
@@ -1,23 +1,44 @@
 package Middle.FirstVolumeWorlds;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
         String text = "Идут три брата: брат Клим брит, брат Глеб брит, брат Игнат бородат." ;
 
-        Map<String, Integer> wordMap = new HashMap<>();
+        text = text.replaceAll("[^a-zA-Zа-яА-Я0-9\\s]", "").toLowerCase();
+        String[] words = text.split("\\s+");
 
-        for (String word : lines) {
-            word = word.replaceAll("[^A-Za-zА-Яа-я]", "").toLowerCase();
-            String[] arr = word.split(" ");
-            for (String i : arr) {
-                if (wordMap.containsKey(i)){
-                    wordMap.put(i, word.get(i) + 1);
-                }
-            }
-                System.out.println(entry.getKey() + " [" + entry.getValue() + "]");
-            }
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
         }
+//        for (String word : words) {
+//            if (wordMap.containsKey(word)){
+//
+//                System.out.println(word + "[" + wordMap.get(word) + "]");
+//                wordMap.remove(word);
+//            }
+//        }
+
+        Map<String, Integer> sortedMap = wordMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+        sortedMap.forEach((word, count) -> System.out.println(word + "[" + count + "]"));
+
     }
+
+
+}
