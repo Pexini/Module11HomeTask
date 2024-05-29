063bb5a8047c2310a88194081b12bf39f415da7e
Pavel Davydov <Davydov.works@gmail.com>
2024-05-29 02:42:57 -0400

    Second UniqueNumbers

--- src/main/java/Middle/UnicumNumbers/Main.java
+++ src/main/java/Middle/UnicumNumbers/Main.java
@@ -1,4 +1,23 @@
 package Middle.UnicumNumbers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+// “Уникальные числа”
+//       Вводится n целых чисел. Определите, сколько различных чисел было введено.
+//        Например: 1 3 3 5 6 7 7 7
+//        Ответ: 5
+//        Используйте для решения задачи коллекции.
+
+        List<Integer> list = new ArrayList<>(List.of(1, 3, 3, 5, 6, 7, 7, 7));
+        Set<Integer> uniquenumbers = new HashSet<>(list);
+
+        System.out.println("sum of unique numbers " + uniquenumbers.size());
+        System.out.println("unique numbers " + uniquenumbers);
+
+    }
 }
